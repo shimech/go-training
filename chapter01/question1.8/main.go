8523f98107344459961b8178071ba8e71f49f82b
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-11-06 22:06:28 +0900

    練習問題1.8

---
+++ chapter01/question1.8/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !isValidUrl(url) {
+			fmt.Fprintf(os.Stderr, "url: %s is invalid.\n", url)
+			continue
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+
+}
+
+func isValidUrl(url string) bool {
+	return strings.HasPrefix(url, "https://")
+}
