eba076e24ad48153e0c54005ecc0e450e442f922
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-11-18 01:02:31 +0900

    練習問題1.10

--- chapter01/question1.10/main.go
+++ chapter01/question1.10/main.go
@@ -15,9 +15,14 @@ func main() {
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch)
 	}
+	logs := []string{}
 	for range os.Args[1:] {
-		fmt.Println(<-ch)
+		log := <-ch
+		fmt.Println(log)
+		logs = append(logs, fmt.Sprintf("%s\n", log))
 	}
+	filename := fmt.Sprintf("%s.log", time.Now().Format("01_02_15_04_05"))
+	writeLog(filename, logs)
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
@@ -37,4 +42,22 @@ func fetch(url string, ch chan<- string) {
 	}
 	secs := time.Since(start).Seconds()
 	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+func writeLog(filename string, lines []string) error {
+	filepath := fmt.Sprintf("./log/%s", filename)
+	file, err := os.Create(filepath)
+	if err != nil {
+		defer file.Close()
+		return err
+	}
+	for _, line := range lines {
+		b := []byte(line)
+		_, err := file.Write(b)
+		if err != nil {
+			defer file.Close()
+			return err
+		}
+	}
+	return nil
 }
