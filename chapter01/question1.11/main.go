b67dec7bd7b953c3ebcc1e142c7e35ed04599ba8
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-12-04 01:04:00 +0900

    練習問題1.11

--- chapter01/question1.11/main.go
+++ chapter01/question1.11/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -12,17 +13,14 @@ import (
 func main() {
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := range os.Args[1:] {
+	filepath := "./txt/urls.txt"
+	urls := readUrls(filepath)
+	for _, url := range urls {
 		go fetch(url, ch)
 	}
-	logs := []string{}
-	for range os.Args[1:] {
-		log := <-ch
-		fmt.Println(log)
-		logs = append(logs, fmt.Sprintf("%s\n", log))
+	for range urls {
+		fmt.Println(<-ch)
 	}
-	filename := fmt.Sprintf("%s.log", time.Now().Format("01_02_15_04_05"))
-	writeLog(filename, logs)
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
@@ -44,20 +42,16 @@ func fetch(url string, ch chan<- string) {
 	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
 
-func writeLog(filename string, lines []string) error {
-	filepath := fmt.Sprintf("./log/%s", filename)
-	file, err := os.Create(filepath)
+func readUrls(filepath string) []string {
+	file, err := os.Open(filepath)
 	if err != nil {
-		defer file.Close()
-		return err
+		fmt.Fprint(os.Stderr, err)
 	}
-	for _, line := range lines {
-		b := []byte(line)
-		_, err := file.Write(b)
-		if err != nil {
-			defer file.Close()
-			return err
-		}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	urls := []string{}
+	for scanner.Scan() {
+		urls = append(urls, scanner.Text())
 	}
-	return nil
+	return urls
 }
