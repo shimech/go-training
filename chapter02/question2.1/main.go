44310b80b55e0dffc69f8912acb7854d143009fe
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-12-08 00:46:56 +0900

    練習問題2.1

--- chapter02/question2.1/main.go
+++ chapter02/question2.1/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/shimech/go-training/chapter02/question2.1/tempconv"
 )
 
 func main() {
-	fmt.Printf("%g\n", tempconv.AbsoluteZeroC)
+	temp, err := strconv.ParseFloat(os.Args[1], 64)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(tempconv.CToK(tempconv.Celsius(temp)).String())
 }
--- chapter02/question2.1/tempconv/conv.go
+++ chapter02/question2.1/tempconv/conv.go
@@ -2,3 +2,13 @@ package tempconv
 
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+func CToK(c Celsius) Kelvin     { return Kelvin(c - AbsoluteZeroC) }
+func KToC(k Kelvin) Celsius     { return Celsius(k + Kelvin(AbsoluteZeroC)) }
+func FToK(f Fahrenheit) Kelvin {
+	c := FToC(f)
+	return CToK(c)
+}
+func KToF(k Kelvin) Fahrenheit {
+	c := KToC(k)
+	return CToF(c)
+}
