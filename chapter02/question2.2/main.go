ff61c7e979d6dc1420cb018d8e126f852c12ac5e
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-12-08 23:56:47 +0900

    コマンドライン引数を受け取るやつ実装

--- chapter02/question2.2/main.go
+++ chapter02/question2.2/main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 
+	"github.com/shimech/go-training/chapter02/question2.2/lengthconv"
 	"github.com/shimech/go-training/chapter02/question2.2/tempconv"
+	"github.com/shimech/go-training/chapter02/question2.2/weightconv"
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
+	tArg := flag.String("temp", "", "Temperature")
+	lArg := flag.String("length", "", "Length")
+	wArg := flag.String("weight", "", "Weight")
+	flag.Parse()
+
+	if *tArg != "" {
+		t, err := strconv.ParseFloat(*tArg, 64)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			fmt.Fprintf(os.Stderr, "%v\n", err)
 			os.Exit(1)
 		}
 		f := tempconv.Fahrenheit(t)
 		c := tempconv.Celsius(t)
 		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+
+	if *lArg != "" {
+		l, err := strconv.ParseFloat(*lArg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		f := lengthconv.Feet(l)
+		m := lengthconv.Meter(l)
+		fmt.Printf("%s = %s, %s = %s\n", f, lengthconv.FToM(f), m, lengthconv.MToF(m))
+	}
+
+	if *wArg != "" {
+		w, err := strconv.ParseFloat(*wArg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		g := weightconv.Gram(w)
+		p := weightconv.Pound(w)
+		fmt.Printf("%s = %s, %s = %s\n", g, weightconv.GToP(g), p, weightconv.PToG(p))
 	}
 }
