b4468036c4bde27f853fc322ee49ba05724d7f3d
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-12-10 11:12:57 +0900

    テストコード追加

--- chapter02/question2.5/popcount/popcount_test.go
+++ chapter02/question2.5/popcount/popcount_test.go
@@ -2,6 +2,23 @@ package popcount
 
 import "testing"
 
+var tests = []struct {
+	input uint64
+	want  int
+}{
+	{0, 0},
+	{1, 1},
+	{3, 2},
+}
+
+func TestPopCountLogicalAndDecrement(t *testing.T) {
+	for _, test := range tests {
+		if got := PopCountLogicalAndDecrement(test.input); got != test.want {
+			t.Errorf("PopCountLogicalAndDecrement(%d) = %d", test.input, got)
+		}
+	}
+}
+
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		PopCount(uint64(i))
