fec783507e7ddaffff5a14799045963ddd0fc2b6
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-12-11 00:23:49 +0900

    練習問題2.4

---
+++ chapter02/question2.4/popcount/popcount.go
@@ -0,0 +1,29 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountBitShift(x uint64) int {
+	c := 0
+	for i := 0; i < 64; i++ {
+		b := byte((x >> i) & 1)
+		c += int(b)
+	}
+	return c
+}
